1f27f6993ba6523b47fa01625b09c92a877a2ee8
Ching Chow <ching.chow.sc@gmail.com>
2013-05-25 05:31:20 -0400

    Solution 8 in Python.

---
+++ problem_008.py
@@ -0,0 +1,14 @@
+with open("problem_008.input", "r") as f:
+    big_number_str = "".join(line.strip() for line in f.readlines())
+
+fives = []
+i = 0
+while i + 5 <= len(big_number_str):
+    fives.append(sorted(big_number_str[slice(i, i + 5)], reverse=True))
+    i += 1
+
+product = 1
+for n in sorted(fives).pop():
+    product *= int(n)
+
+print(product)
