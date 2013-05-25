fb93f814165e97e9f7e49bfa52abfc1c1685ef57
Ching Chow <ching.chow.sc@gmail.com>
2013-05-25 03:56:18 -0400

    Improved upperbound for trial division.

--- problem_005.py
+++ problem_005.py
@@ -1,6 +1,6 @@
 def is_prime(n):
     """Determine if n is prime."""
-    for i in range(2, n // 2 + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0: return False
     return True
 
